c040474c7440cbe62d1877f0c488c8764ad432e3
MadMan24 <mdverdera@yahoo.com>
2022-11-12 10:31:19 +0800

    Fix ad sense script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,19 @@ import Script from "next/script";
 const Document = () => {
   return (
     <Html lang="en" className="scroll-smooth">
-      <Head />
-      <Script
-        id="Adsense-id"
-        data-ad-client="ca-pub-4037215281786151"
-        async
-        strategy="beforeInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      />
+      <Head>
+        <Script
+          id="Adsense-id"
+          data-ad-client="ca-pub-4037215281786151"
+          async
+          strategy="beforeInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          onError={(e) => {
+            console.error("Script failed to load", e);
+          }}
+        />
+      </Head>
+
       <body className="overflow-x-hidden">
         <Main />
         <NextScript />
